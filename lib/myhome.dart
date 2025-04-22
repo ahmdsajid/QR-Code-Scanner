d0be4bfcc8aeba4d57ad50c6295e4a0255ae0723
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2025-04-23 00:08:01 +0600

    update

--- lib/myhome.dart
+++ lib/myhome.dart
@@ -18,8 +18,12 @@ class _MyhomeState extends State<Myhome> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [ElevatedButton(onPressed: () {}, child: Text('Scan QR Code',style: TextStyle(color: Colors.blue),))],
+          children: [ElevatedButton(onPressed: () {}, child: Text('Scan QR Code',style: TextStyle(fontSize: 20,color: Colors.blue),)),
+          SizedBox(height: 20,),
+          ElevatedButton(onPressed: (){}, child: Text('Build QR', style: TextStyle(fontSize: 20, color: Colors.blue),))
+          ],
         ),
+        
       ),
     );
   }
