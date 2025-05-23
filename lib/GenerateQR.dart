3228275b5f1aa890f8a5a810de3a77ef51c64e68
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2025-05-23 23:51:41 +0600

    update

--- lib/GenerateQR.dart
+++ lib/GenerateQR.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class GenerateQR extends StatefulWidget {
   const GenerateQR({super.key});
@@ -8,6 +9,7 @@ class GenerateQR extends StatefulWidget {
 }
 
 class _GenerateQRState extends State<GenerateQR> {
+  TextEditingController urlcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +20,30 @@ class _GenerateQRState extends State<GenerateQR> {
         ),
       
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Generate QR code')
-        ],),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if(urlcontroller.text.isNotEmpty)
+              QrImageView(data: urlcontroller.text, size: 200,),
+               SizedBox(height: 10,),
+               Container(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  controller: urlcontroller,
+                  decoration: InputDecoration(
+                    hintText: "Enter your data",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                    labelText: "Enter your data"
+                  ),
+                ),
+               ),
+               SizedBox(height: 10,),
+               ElevatedButton(onPressed: (){setState(() {
+                 
+               });}, child: Text('Generate QR code'))
+          ],),
+        ),
       ),
     );
   }
