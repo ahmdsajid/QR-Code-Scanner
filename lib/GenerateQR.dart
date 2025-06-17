180941cd2a40fc9a423e647187c057a9daa80956
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2025-06-18 00:54:12 +0600

    update

--- lib/GenerateQR.dart
+++ lib/GenerateQR.dart
@@ -26,7 +26,7 @@ class _GenerateQRState extends State<GenerateQR> {
             children: [
               if(urlcontroller.text.isNotEmpty)
               QrImageView(data: urlcontroller.text, size: 200,),
-               SizedBox(height: 10,),
+               SizedBox(height: 40,),
                Container(
                 padding: EdgeInsets.only(left: 10, right: 10),
                 child: TextField(
