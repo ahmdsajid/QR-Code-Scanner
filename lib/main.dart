b0cf7252705aa10c20c4a2ed01dbd93744b8ceb9
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2025-08-15 04:28:53 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code/myhome.dart';
+import 'package:qr_code/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Myhome(),
+      home: const splashscreeen(),
     );
   }
 }
---
+++ lib/splash.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code/myhome.dart';
+
+class splashscreeen extends StatelessWidget {
+  const splashscreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Positioned(
+              top: -150,
+              right: -100,
+              child: Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    shape: BoxShape.circle),
+              )),
+          Column(
+            children: [
+              //top qr section
+              Expanded(
+                  flex: 2,
+                  child: Center(
+                    child: Icon(
+                      Icons.qr_code_2,
+                      color: Colors.white,
+                      size: 160,
+                    ),
+                  )),
+              //bottom white section
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(60),
+                            topRight: Radius.circular(60))),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Get Started",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          child: Text(
+                            "Your all-in-one solution for scanning and generating qr code- easy, fast and secure",
+                            textAlign: TextAlign.center,
+                            style:
+                                TextStyle(color: Colors.black87, fontSize: 14),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+
+                        //circuler button
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> Myhome()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: CircleBorder(),
+                                padding: EdgeInsets.all(18),
+                                backgroundColor: Colors.amber[700],
+                                elevation: 6),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.black,
+                            ))
+                      ],
+                    ),
+                  ))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
